6b7a7ef1b5f4970dc1b2a24ceae5cbbd46e2c6ed
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-03-04 00:24:38 +0600

    date added

---
+++ math.js
@@ -0,0 +1,19 @@
+// const math = {
+//     min: function min(num1,num2){
+//         return num2
+//     }
+// }
+
+// console.log(Math.min);
+
+const min = Math.min(45,21,85,99,126,5)
+console.log(min); 
+console.log(Math.PI);
+console.log(Math.abs(-88));
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+console.log(Math.floor(2.85));
+console.log(Math.ceil(2.85));
+console.log(Math.random())
+console.log(Math.random()*10)
+
